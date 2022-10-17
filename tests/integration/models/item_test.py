0107a9a9bda9ba87b3f0879f484eb27a5aabd79b
Wojciech Brzyszcz <wbrzyszc@cisco.com>
2022-10-17 11:57:29 +0200

    Added email notifications to Travis

--- tests/integration/models/item_test.py
+++ tests/integration/models/item_test.py
@@ -6,7 +6,7 @@ from tests.base_test import BaseTest
 class ItemTest(BaseTest):
     def test_crud(self):
         with self.app_context():
-            StoreModel('tast').save_to_db()
+            StoreModel('test').save_to_db()
             item = ItemModel('test', 19.99, 1)
 
             self.assertIsNone(ItemModel.find_by_name('test'),
